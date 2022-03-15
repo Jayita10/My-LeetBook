ee60bdb6cdea59340dfb59c278383e1bae54a506
Jayita Pramanik <jayitapramanik2018@gmail.com>
2022-03-15 18:28:17 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (91.78%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,38 +1,11 @@
 class Solution {
 public:
-    int solve(ListNode* head, int k){
-        ListNode* temp = head;
-        ListNode* pre = NULL;
-        
-        while(head->next != head){
-            for(int i=1;i<k;i++){
-                pre = temp;
-                temp = temp->next;
-            }
-            if(temp == head){
-                head = head->next;
-            }
-            pre->next = temp->next;
-            delete(temp);
-            temp = pre->next;
-        }
-        return head->val;
+    int solve(int n, int k){
+        if(n == 1)
+            return 0;
+        return (solve(n-1,k) + k) % n;
     }
-    
     int findTheWinner(int n, int k) {
-        if(k == 1) return n;
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
-        for(int i=1;i<=n;i++){
-            ListNode* node = new ListNode(i);
-            if(head == NULL)
-                head = tail = node;
-            else{
-                tail->next = node;
-                tail = node;
-            }
-            tail->next = head;
-        }
-        return solve(head,k);
+        return solve(n,k)+1;
     }
 };
