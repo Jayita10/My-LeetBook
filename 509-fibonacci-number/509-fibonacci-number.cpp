09637f180412dab4aeea405be77b270b82ac3979
Jayita Pramanik <jayitapramanik2018@gmail.com>
2022-03-17 12:06:45 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (9.65%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,16 @@
 class Solution {
-public:
-    int fib(int n) {
-        //recursion
-        
+    int solve(int n, vector<int> &dp){
         if(n<=1)
             return n;
+        if(dp[n] != -1)
+            return dp[n];
         
-        return fib(n-1)+fib(n-2);
+        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+    }
+public:
+    int fib(int n) {
+        //top-down
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
