6b00709f891145c1cc7791f540ac7fc193484e82
Jayita Pramanik <jayitapramanik2018@gmail.com>
2022-03-17 12:58:19 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (5.83%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
-public:
-    int tribonacci(int n) {
-        //bottom up space  optimaization
+    /**
+    //bottom up space  optimaization
         if(n<=1)
             return n;
         if(n == 2)
@@ -17,5 +16,19 @@ public:
             prev1 = curr_i;
         }
         return prev1;
+        **/
+    int solve(int n, vector<int> &dp){
+        if(n == 0) return 0;
+        if(n == 2 || n == 1) return 1;
+        
+        if(dp[n] != -1) return dp[n];
+        
+        return dp[n] = solve(n-1,dp) + solve(n-2,dp) + solve(n-3,dp);
+    }
+public:
+    int tribonacci(int n) {
+        //top dowm memoisation
+        vector<int> dp(n+1, -1);
+        return solve(n,dp);
     }
 };
