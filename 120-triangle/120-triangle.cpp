6f027bee6c99026ae8d4c69dbeb529a89d3e1629
Jayita Pramanik <jayitapramanik2018@gmail.com>
2022-04-15 11:52:56 +0530

    Time: 12 ms (26.97%), Space: 9.1 MB (22.85%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -14,10 +14,17 @@ public:
         int row = triangle.size();
         int col = triangle[row-1].size();
         vector<vector<int>> dp(row, vector<int>(col,-1));
-        return solve(0,0,row,col,triangle,dp);
+        //return solve(0,0,row,col,triangle,dp);
+        //base case, in last row, there will be "col" possible base cases
+        for(int j = 0;j < col;j++){
+            dp[row-1][j] = triangle[row-1][j];
+        }
         
-        //dp[0][0] = triangle[0][0];
-        
-        //for(int i=1)
+        for(int i = row-2; i >= 0; i--){
+            for(int j = i; j>= 0 ;j--){
+                dp[i][j] = min(triangle[i][j] + dp[i+1][j], triangle[i][j] + dp[i+1][j+1]);    
+            }
+        }
+        return dp[0][0];
     }
 };
