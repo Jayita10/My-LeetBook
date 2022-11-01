f46892e1480d031eaaff7fb124cec69cfd6fc6ee
Jayita Pramanik <jayitapramanik2018@gmail.com>
2022-11-01 23:01:47 +0530

    Time: 10 ms (89.92%), Space: 8.1 MB (8.06%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -4,7 +4,6 @@ public:
         int n = moves.size();
         int x = 0, y = 0;
         
-        int u = 0, d = 0, l = 0, r = 0;
         for(int i=0;i<n;i++){
             if(moves[i] == 'U') y++;
             if(moves[i] == 'D') y--;
@@ -12,7 +11,6 @@ public:
             if(moves[i] == 'R') x++;
         }
         
-        if(y == 0 && x == 0) return true;
-        return false;
+        return (y == 0 && x == 0);
     }
 };
